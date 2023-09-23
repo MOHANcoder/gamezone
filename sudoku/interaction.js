f3fe347916735b22654160b01b1539707b9c412b
MOHANcoder <101055189+MOHANcoder@users.noreply.github.com>
2023-09-23 17:59:15 +0530

    Folder structure changed

--- sudoku/interaction.js
+++ sudoku/interaction.js
@@ -29,7 +29,7 @@ function showInfo() {
         Swal.fire(
             'Info',
             `
-            <img width="100%" src="sudoku-img.jpg" />
+            <img width="100%" src="./assets/sudoku-img.jpg" />
             <strong><big>Sudoku</big></strong> is one of the most popular puzzle games of all time. The goal of Sudoku is to fill a 9×9 grid with numbers so that each row, column and 3×3 section contain all of the digits between 1 and 9. As a logic puzzle, Sudoku is also an excellent brain game. If you play Sudoku daily, you will soon start to see improvements in your concentration and overall brain power. Start a game now. Within no time Sudoku free puzzles will be your favorite online game.
             <a href="https://en.wikipedia.org/wiki/Sudoku">Learn more</a>
             `
